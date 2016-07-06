6f3ee36e9d7290bef34135a0fb79988c065690b2
truongtechno <tranxuantruong1992@gmail.com>
2016-07-06 23:32:54 +0700

    xxx

--- www/EncryptMp3.js
+++ www/EncryptMp3.js
@@ -1,24 +1,24 @@
-// var exec = require('cordova/exec');
 
-// exports.coolMethod = function(arg0, success, error) {
-//     exec(success, error, "EncryptMp3", "coolMethod", [arg0]);
-// };
+var exec = cordova.require('cordova/exec');
 
 var ksencypt = {
 	encypt : function(file, action, callback) {
 		var success = function(result) {
 			callback(result);
 		};
+		
 		var fail = function(error) {
 			callback(error);
 			alert("Not Ok:" + error);
 		}
 
-		cordova.exec(success, // success callback function
-		fail, // error callback function
-		'EncryptMp3', // mapped to our native Java class called "Calendar"
-		action, // with this action name
-		[ file ]);
+		exec(
+		  success, // success callback function
+		  fail, // error callback function
+		  'KSEncrypt', // mapped to our native Java class called "Calendar"
+		  action, // with this action name
+		  [ file ]);
 	}
 }
+
 module.exports = ksencypt;
